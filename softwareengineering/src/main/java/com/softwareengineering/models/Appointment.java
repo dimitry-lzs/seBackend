19436ba136c8093937f8d5e71c2177fccc8c80c9
dimitry-lzs <lazanasdimitrios@gmail.com>
2025-05-06 11:16:46 +0300

    i am committed

--- softwareengineering/src/main/java/com/softwareengineering/models/Appointment.java
+++ softwareengineering/src/main/java/com/softwareengineering/models/Appointment.java
@@ -6,9 +6,11 @@ import com.softwareengineering.models.enums.Status;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.Table;
 
-
 @Table("appointments")
 public class Appointment extends Model {
+    public Appointment() {
+    }
+
     public Appointment(Timestamp date, Time timeFrom, Time timeTo, Status status, int patientID, int doctorID) {
         this.set("date", date);
         this.set("timeFrom", timeFrom);
@@ -25,33 +27,43 @@ public class Appointment extends Model {
     public Time getTimeFrom() {
         return (Time) this.get("timeFrom");
     }
+
     public Time getTimeTo() {
         return (Time) this.get("timeTo");
     }
+
     public Status getStatus() {
         return (Status) this.get("status");
     }
+
     public int getPatientID() {
         return (int) this.get("patientID");
     }
+
     public int getDoctorID() {
         return (int) this.get("doctorID");
     }
+
     public void setDate(Timestamp date) {
         this.set("date", date);
     }
+
     public void setTimeFrom(Time timeFrom) {
         this.set("timeFrom", timeFrom);
     }
+
     public void setTimeTo(Time timeTo) {
         this.set("timeTo", timeTo);
     }
+
     public void setStatus(Status status) {
         this.set("status", status);
     }
+
     public void setPatientID(int patientID) {
         this.set("patientID", patientID);
     }
+
     public void setDoctorID(int doctorID) {
         this.set("doctorID", doctorID);
     }
