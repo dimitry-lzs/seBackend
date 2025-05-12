ee914c76bcdc1128f4ee3f4579231f160a8ac2dc
Tsalmpo <antonis.tsal@gmail.com>
2025-05-12 10:46:35 +0300

    introduce set-rating

--- softwareengineering/src/main/java/com/softwareengineering/dto/RatingBody.java
+++ softwareengineering/src/main/java/com/softwareengineering/dto/RatingBody.java
@@ -3,6 +3,6 @@ package com.softwareengineering.dto;
 public class RatingBody {
     public int stars;
     public String comments;
-    public int doctorId;
-    public int patientId;
+    public Integer doctorID;
+    public Integer patientID;
 }
--- softwareengineering/src/main/java/com/softwareengineering/services/RatingsService.java
+++ softwareengineering/src/main/java/com/softwareengineering/services/RatingsService.java
@@ -9,4 +9,13 @@ public class RatingsService {
         List<Map<String, Object>> ratings = Rating.where("doctorID = ?", doctorID).toMaps();
         return ratings;
     }
+
+    public static void setRating(int stars, String comments, int patientID, int doctorID) {
+        Rating rating = new Rating();
+        rating.set("stars", stars);
+        rating.set("comments", comments);
+        rating.set("patientID", patientID);
+        rating.set("doctorID", doctorID);
+        rating.saveIt();
+    }
 }
