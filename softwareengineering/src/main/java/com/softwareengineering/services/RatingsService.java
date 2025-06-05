c8e36aefcf89df6ae1ff1a179d56f5b05f70cdd8
Tsalmpo <antonis.tsal@gmail.com>
2025-06-05 18:59:55 +0300

    add calculate rating functionality after every set rating occurs

--- softwareengineering/src/main/java/com/softwareengineering/services/RatingsService.java
+++ softwareengineering/src/main/java/com/softwareengineering/services/RatingsService.java
@@ -3,6 +3,7 @@ package com.softwareengineering.services;
 import java.util.List;
 import java.util.Map;
 import com.softwareengineering.models.Rating;
+import com.softwareengineering.models.User;
 
 public class RatingsService {
     public static List<Map<String, Object>> getRatings(int doctorID) {
@@ -17,5 +18,27 @@ public class RatingsService {
         rating.set("patientID", patientID);
         rating.set("doctorID", doctorID);
         rating.saveIt();
+    }
+
+    public static void calcRating(int doctorID) {
+        List<Map<String, Object>> ratings = Rating.where("doctorID = ?", doctorID).toMaps();
+        if (ratings.isEmpty()) {
+            return; // No ratings available
+        }
+        double totalStars = 0;
+        for (Map<String, Object> rating : ratings) {
+            totalStars += (int) rating.get("stars");
+        }
+        double stars = totalStars / ratings.size();
+
+        // Find the doctor in the users table and update their rating
+        User doctor = User.findFirst("id = ?", doctorID);
+        if (doctor != null) {
+            doctor.set("rating", stars);
+            doctor.saveIt();
+        } else {
+            System.out.println("Doctor with ID " + doctorID + " not found");
+        }
+
     }
 }
