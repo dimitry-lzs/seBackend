9c626f0351eca15d78820e66833a9030d2a2c062
dimitry-lzs <lazanasdimitrios@gmail.com>
2025-06-24 16:34:57 +0300

    add constraint

--- sql/ratings.sql
+++ sql/ratings.sql
@@ -1,6 +1,7 @@
 CREATE TABLE ratings (
 	stars INTEGER,
 	comments TEXT,
-	patientID INTEGER REFERENCES users(id),
-	doctorID INTEGER REFERENCES users(id)
+	patientID INTEGER REFERENCES users(id) NOT NULL,
+	doctorID INTEGER REFERENCES users(id) NOT NULL,
+	appointmentID INTEGER REFERENCES appointments(id) NOT NULL
 );
